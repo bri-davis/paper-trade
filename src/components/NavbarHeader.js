553e2c6e605087da210d093e612ada943d6b234d
Brian Davis <briandavis@brians-mbp.mynetworksettings.com>
2022-11-16 23:33:16 -0500

    Created login component

--- src/components/NavbarHeader.js
+++ src/components/NavbarHeader.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import About from './pages/About';
 import Home from './pages/Home';
+import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
 
 
@@ -23,6 +23,7 @@ export default class NavbarHeader extends Component {
             <Nav className="me-auto">
                 <Nav.Link as={Link} to="/home">Home</Nav.Link>
                 <Nav.Link as={Link} to="/about">About</Nav.Link>
+                <Nav.Link as={Link} to="/login">Login</Nav.Link>
             </Nav>
             </Navbar.Collapse>
         </Container>
@@ -31,8 +32,9 @@ export default class NavbarHeader extends Component {
       </div>
         <Routes>
             <Route index element={<Home />} />
-            <Route path="home" element={<Home />} />                    
+            <Route path="home" element={<Home Login />} />                    
             <Route path="about" element={<About />} />
+            <Route path="login" element={<Login />} />
             <Route path="*" element={<PageNotFound />} />
         </Routes>
     </BrowserRouter>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import Login from './Login';
 
 export default function Home() {
-  return <h2>Home</h2>;
+  return <div>
+    <Login />
+  </div>;
 }
