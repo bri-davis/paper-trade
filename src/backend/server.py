9a2fb672be532d57f585c04f8e0dfbcd88d928fe
Brian Davis <brian.michael.davis@temple.edu>
2022-12-20 17:37:15 -0500

    Added backend function for creating stocklist

--- src/backend/server.py
+++ src/backend/server.py
@@ -5,10 +5,32 @@ app = Flask(__name__)
 CORS(app)
 
 import yfinance as yf
-import random
 import boto3
 import botocore
+import json
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/create/', methods=['GET', 'POST'])
+def createStockList():
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('paper-trade')
+
+    content = request.json
+
+    try:
+        table.put_item(
+            Item={
+                    'userId': content['username'],
+                    'stockList': []
+            },
+            ConditionExpression = 'attribute_not_exists(userId)'
+    )
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] != 'ConditionalCheckFailedException':
+            raise Exception('User creation error')
+
+    return str(table.item_count)
+
