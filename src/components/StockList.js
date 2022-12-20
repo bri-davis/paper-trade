d477cb58d700e1e8ebd21b7ef6c4e43faf9958c8
Brian Davis <brian.michael.davis@temple.edu>
2022-12-20 17:46:10 -0500

    Added dynamic display table for viewing stocks / prices

--- src/components/StockList.js
+++ src/components/StockList.js
@@ -1,33 +1,58 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Form from 'react-bootstrap/Form';
 import { Table } from "react-bootstrap";
+//import Table from 'react-bootstrap/Table';
 
-export default function StockList() {
+
+export default function StockList({stocks}) {
+
+  /*const [data, setData] = useState([]);
+  setData({stocks});
+
+  const user = { username: "settest2" };
+
+  const fetchData = () => {
+      fetch('http://127.0.0.1:5000/prices/', {
+        method: 'POST', // or 'PUT'
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+      })
+      .then((response) => response.json())
+      .then((actualData) => {
+        console.log(actualData);
+        setData(actualData);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  */
+
     return (
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
-          </tr>
-        </thead>
+      <div>
+        <Table striped bordered hover>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
+        <tr>
+          <th>Ticker</th>
+          <th>Price</th>
+        </tr>
+        {stocks.map((item, index) => (
+          <tr key={index}>
+            <td>{item.Ticker}</td>
+            <td>{item.Price}</td>
           </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-        </tbody>
+        ))}
+      </tbody>
       </Table>
+
+      </div>
     );
   }
   
