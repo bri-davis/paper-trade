cc6a9dd83f9eab1ba41ad61b9e289ffbc923cc59
Brian Davis <brian.michael.davis@temple.edu>
2022-12-20 17:48:01 -0500

    Added state for data from searching/viewing stocklist

--- src/components/TradeWindow.js
+++ src/components/TradeWindow.js
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
 import Form from 'react-bootstrap/Form';
-import { Button } from "react-bootstrap";
-import Searchbar from "./Searchbar";
 import SearchResult from "./SearchResult";
-import StockList from "./StockList";
 
 export default function TradeWindow() {
     const [searchResult, setSearchResult] = useState('');
@@ -11,12 +8,32 @@ export default function TradeWindow() {
     const updateSearchResult = (update) => {
         setSearchResult(update);
     }
+
+
+    const [account, setAccount] = useState('');
+    
+      const handleChange = (event) => {
+        setAccount(event.target.value);
+        console.log(account);
+      };
+    
+      const handleSubmit = (event) => {
+        // prevents the submit button from refreshing the page
+        event.preventDefault();    
+        console.log(account);
+      };
+
     return (
         <div className="container">
+
+            <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" name="email" placeholder="Enter email" onChange={handleChange} value={account}/>
+            </Form.Group>
+            </Form>
             {searchResult}
-            <Searchbar  updateSearchResult={updateSearchResult}/>
-            <SearchResult updateSearchResult={searchResult}/>
-            <StockList />
+            <SearchResult account={account}/>
         </div>
     )
 }
