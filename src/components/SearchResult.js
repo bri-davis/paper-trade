aa74cc0e1548325a4b2a7c538d2ab7492712cb77
Brian Davis <brian.michael.davis@temple.edu>
2022-12-20 17:44:05 -0500

    Added frontend handler for removing from stocklist

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -31,6 +31,26 @@ export default function SearchResult({account}) {
 
                 handlePrice();
         };
+
+        function handleSell() {
+            const data = { username: account, ticker: search};
+            fetch('http://127.0.0.1:5000/remove/', {
+                method: 'POST', // or 'PUT'
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+              })
+                .then((response) => response.json())
+                .then((data) => {
+                  console.log('Success:', data);
+                })
+                .catch((error) => {
+                  console.error('Error:', error);
+                });
+            
+                handlePrice();
+        };
     
     return (
         <div>
